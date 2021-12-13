4fc020742e5ef21a921ad6577c305cce1ec23ab6
Duvans <duvan.jayasan@gmail.com>
2021-12-13 11:09:25 +0700

    push

---
+++ server.js
@@ -0,0 +1,24 @@
+function requireHTTPS(req, res, next)
+{
+    if(!req.secure && req.get('x-forwarded-proto') !== 'https')
+    {
+        return res.redirect('https://' + req.get('host') + req.url)
+    }
+    next()
+}
+
+const express = require('express')
+const app = express()
+
+//penerapan middleware
+// app.use(requireHTTPS)
+app.use(express.static('./dist/test-angular-deployment'))
+
+//proses handle request->method get
+app.get('/*', (req, res) => req.sendFile('index.html', {root: 'dist/test-angular-deployment'}))
+
+const port = process.env.PORT || 8000
+app.listen(port, () => 
+{
+    console.log(`Example app listening att http://localhost:${port}`)
+})
